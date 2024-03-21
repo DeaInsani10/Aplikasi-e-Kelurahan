27f8b374f07f039281018668fc975cccaf08f37f
DeaInsani10 <deainsani1001@gmail.com>
2024-03-21 13:24:44 +0700

    commit awal

---
+++ resources/views/listSKK.blade.php
@@ -0,0 +1,32 @@
+<div class="d-flex justify-content-between flex-wrap flex-md-nowrap align-items-center pt-3 pb-2 mb-3 border-bottom">
+    <h1 class="h2">Data Permohonan Surat Keterangan Kematian</h1>
+</div>
+
+<table>
+    <tr>
+      <th>No</th>
+      <th>Tanggal Pengajuan</th>
+      <th>Nama Pemohon</th>
+      <th>Status</th>
+      <th>Aksi</th>
+    </tr>
+    <tr>
+      <td>1</td>
+      <td>Senin, 19 Maret 2024</td>
+      <td>Budi Setiawan</td>
+      <td>Menunggu Verifikasi</td>
+    </tr>
+    <tr>
+        <td>1</td>
+        <td>Senin, 19 Maret 2024</td>
+        <td>Budi Setiawan</td>
+        <td>Menunggu Verifikasi</td>
+      </tr>
+      <tr>
+        <td>1</td>
+        <td>Senin, 19 Maret 2024</td>
+        <td>Budi Setiawan</td>
+        <td>Menunggu Verifikasi</td>
+      </tr>
+
+  </table>
---
+++ resources/views/sidebar.blade.php
@@ -0,0 +1,74 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="utf-8" />
+        <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
+        <meta name="description" content="" />
+        <meta name="author" content="" />
+        <title>Dasboard Verifikator</title>
+        <link href="/css/styles.css" rel="stylesheet" />
+        <script src="https://use.fontawesome.com/releases/v6.3.0/js/all.js" crossorigin="anonymous"></script>
+    </head>
+    <body>
+        <div id="layoutSidenav">
+            <div id="layoutSidenav_nav">
+                <nav class="sb-sidenav accordion sb-sidenav" id="sidenavAccordion">
+                    <div class="sb-sidenav-menu">
+                        <div class="nav">
+                            <a class="nav-link" href="index.html">
+                                <div class="sb-nav-link-icon"><i class="fas fa-tachometer-alt"></i></div>
+                                Dashboard
+                            </a>
+                            <a class="nav-link collapsed" href="#" data-bs-toggle="collapse" data-bs-target="#collapseLayouts" aria-expanded="false" aria-controls="collapseLayouts">
+                                <div class="sb-nav-link-icon"><i class="fas fa-columns"></i></div>
+                                Data Permohonan
+                                <div class="sb-sidenav-collapse-arrow"><i class="fas fa-angle-down"></i></div>
+                            </a>
+                            <div class="collapse" id="collapseLayouts" aria-labelledby="headingOne" data-bs-parent="#sidenavAccordion">
+                                <nav class="sb-sidenav-menu-nested nav">
+                                    <a class="nav-link" href="layout-static.html">SKK</a>
+                                    <a class="nav-link" href="layout-sidenav-light.html">SKU</a>
+                                    <a class="nav-link" href="layout-sidenav-light.html">SKD</a>
+                                    <a class="nav-link" href="layout-sidenav-light.html">SKBN</a>
+                                </nav>
+                            </div>
+
+                            <a class="nav-link" href="tables.html">
+                                <div class="sb-nav-link-icon"><i class="fas fa-table"></i></div>
+                                Tables
+                            </a>
+                        </div>
+                    </div>
+                    <div class="sb-sidenav-footer">
+                        <div class="small">Logged in as:</div>
+                        Start Bootstrap
+                    </div>
+                </nav>
+            </div>
+            <div id="layoutSidenav_content">
+                <main>
+                    <div class="container-fluid px-4">
+                        <h1 class="mt-4">Static Navigation</h1>
+                        <ol class="breadcrumb mb-4">
+                            <li class="breadcrumb-item"><a href="index.html">Dashboard</a></li>
+                            <li class="breadcrumb-item active">Static Navigation</li>
+                        </ol>
+                        {{-- <div class="card mb-4">
+                            <div class="card-body">
+                                <p class="mb-0">
+                                    This page is an example of using static navigation. By removing the
+                                    <code>.sb-nav-fixed</code>
+                                    class from the
+                                    <code>body</code>
+                                    , the top navigation and side navigation will become static on scroll. Scroll down this page to see an example.
+                                </p>
+                            </div>
+                        </div> --}}
+                        <div style="height: 100vh"></div>
+                        <div class="card mb-4"><div class="card-body">When scrolling, the navigation stays at the top of the page. This is the end of the static navigation demo.</div></div>
+                    </div>
+                </main>
+            </div>
+        </div>
+    </body>
